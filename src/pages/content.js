8438fbff349a28f2aa27bd36f9c85a17f4d813bf
gabrielbarth <gabrielbarth98@gmail.com>
2020-06-20 18:12:05 -0400

    chrore: testing pdf from graphql api

--- src/pages/content.js
+++ src/pages/content.js
@@ -4,41 +4,33 @@ import { graphql, useStaticQuery, Link } from 'gatsby'
 import Layout from '../components/Layout/layout'
 import Head from '../components/Layout/head'
 
-const ContentPage = (props) => {
-
-  const data = useStaticQuery(graphql`
-  query {
-    contentfulPdfContent{
-      files {
-        file {
-          url
+export const query = useStaticQuery(graphql`
+query {
+  allContentfulPdfContent {
+    edges {
+      node {
+        files {
+          file {
+            url
+          }
         }
       }
     }
   }
-  `)
+}
+`)
 
-  
-
+const ContentPage = (props) => {
+  console.log(props)
+  console.log(props.data)
   return (
     <Layout>
-      <Head title="Blog" />
+      <Head title="Materiais" />
       <h1>Contents</h1>
-
-      {
-      data.contentfulPdfContent.files.map(file => {
-      // const p = props.contentfulPdfContent.files.file.url + '';
-      const p = file.url + '';
-      const validateProp = p.replace("//", "");
-      
-      return  (
-        <a href={file.url} download>
-          {file.url}
-          download pdf
-        </a>
-      )
-      })
-      }
+     
+          {/* <a href={file.url} download>
+             {'\n'}download pdf
+          </a> */}
 
     </Layout>
   )
